87b602b4a80f8e162fc03f37117e94adbd742e07
Koh-Ko <ekoea2002@gmail.com>
2020-01-11 20:37:14 +0700

    push eko's self

--- epython3.py
+++ epython3.py
@@ -6,18 +6,21 @@ text3 = "jalan - jalan di hari jum'at"
 
 text4 = 'mahmuy berkata, "kemaren kemana bro?"'
 
-text5 = "hobloh menjawab, \"jalan - jalan bro\" "
+text5 = "dobleh menjawab, \"jalan - jalan bro\" "
 
-text6 = 'mahmuy: "kemaren kemana bro?" \nhobloh: "jalan - jalan bro"'
+text6 = 'mahmuy: "kemaren kemana bro?" \mbleh: "jalan - jalan bro"'
 
 text7 = """
 mahmuy: "kemaren kemana bro?"
-hobloh: "jalan - jalan bro"
+dobleh: "jalan - jalan bro"
 mahmuy: "jalan - jalan kemana bro?"
-hobloh: "ke Mang Uing bro!!!!"
+dobleh: "ke Mang Uing bro!!!!"
 """
 
 text8 = r'C:\nyoto' # raw string --> bagaimanakah hasil ini??
+"""Hasilnya berupa string yang sesuai dengan apa yang dituliskan.
+Menekan arti aktual karakter Escape, kode \n diabaikan.
+"""
 
 print(text8)
 print(5*"wk")
